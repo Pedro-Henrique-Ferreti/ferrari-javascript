907b4c035a4e44910f9f6944f76e784168c03469
Pedro Henrique <phfs28@gmail.com>
2021-01-14 23:38:00 -0300

    funcao de login

--- public/assets/scripts/auth.js
+++ public/assets/scripts/auth.js
@@ -1,6 +1,12 @@
+import firebase from './firebase-app';
+import { getFormValues, hideAlertError, showAlertError } from './utils';
+
 const authPage = document.querySelector('main#auth');
 
 if (authPage) {
+
+    const auth = firebase.auth();
+
     const hideAuthForms = () => {
         document.querySelectorAll('#auth form').forEach(el => el.classList.add('hide'));
     }
@@ -30,7 +36,8 @@ if (authPage) {
                 showAuthForm("reset");
                 break;
             default: 
-                showAuthForm("auth-email");
+                // showAuthForm("auth-email");
+                showAuthForm("login");
         }
     };
 
@@ -53,5 +60,37 @@ if (authPage) {
 
         location.hash = "#login";
         btnSubmit.disabled = false;
+    });
+
+    const formAuthRegister = document.querySelector("#register");
+
+    formAuthRegister.addEventListener('submit', e => {
+        e.preventDefault();
+        hideAlertError(formAuthRegister);
+
+        const values = getFormValues(formAuthRegister);
+    
+        auth.createUserWithEmailAndPassword(values.email, values.password)
+        .then(response =>{
+
+            const { user } = response;
+
+            user.updateProfile({ displayName: values.name });
+            window.location.href = "/";
+        })
+        .catch(showAlertError(formAuthRegister))
+    });
+
+    const formAuthLogin = document.querySelector("#login");
+
+    formAuthLogin.addEventListener('submit', e =>{
+        e.preventDefault();
+        hideAlertError(formAuthLogin);
+
+        const values = getFormValues(formAuthLogin);
+
+        auth.signInWithEmailAndPassword(values.email, values.password)
+        .then( resp => window.location.href = "/" )
+        .catch(showAlertError(formAuthLogin))
     });
 }
