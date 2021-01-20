a1169422f46664c5e1d2bb5ce299296dc5d582cc
Pedro Henrique <phfs28@gmail.com>
2021-01-19 23:26:07 -0300

    pagina serviços

--- public/assets/scripts/services.js
+++ public/assets/scripts/services.js
@@ -38,16 +38,6 @@ const renderServiceOptions = (context, serviceOptions) => {
                 });
             }
 
-            serviceSummary.map(id =>serviceOptions.filter(item => (+item.id === +id))[0]).sort((a, b) => {
-                if (a.name > b.name) {
-                    return 1;
-                } else if (a.name < b.name) {
-                    return -1;
-                } else { 
-                    return 0;
-                }
-            });
-
             renderServiceSummary(context, serviceOptions);
         });
     });
@@ -61,6 +51,15 @@ const renderServiceSummary = (context, serviceOptions) => {
     serviceSummary
     .map(id => serviceOptions
     .filter(item => Number(item.id) === Number(id))[0])
+    .sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else if (a.name < b.name) {
+            return -1;
+        } else { 
+            return 0;
+        }
+    })
     .forEach(item => {
         appendTemplate(tbodyEl, "tr", 
         `   <td>${item.name}</td>
